f2bbc741bffdbe5c2334d86093472deaa32be36d
Fátima Lourdes Santos Guzmán <201800240@colsanjose.edu.gt>
2021-06-14 19:55:54 -0600

    Creacion de proyecto.

---
+++ PracticaLS/PracticaLS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaLS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lourdes Santos");
+            Console.WriteLine("4to Baco");
+        }
+    }
+}
